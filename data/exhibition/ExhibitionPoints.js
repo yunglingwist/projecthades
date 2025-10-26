1e1118745fc5e25ebffe2899f10f34d2363324f3
GrandTuvalet <atakankayi2001@gmail.com>
2025-10-26 22:28:49 +0100

    deleted unnecessary lines and files

--- data/exhibition/ExhibitionPoints.js
+++
@@ -1,5 +0,0 @@
-[
-  { "itemId": "obol-coin", "x": 1500, "y": 1000, "icon": "images/ItemObol.webp", "level": "house-of-hades" },
-  { "itemId": "stygian-blade", "x": 62, "y": 85, "icon": "images/ItemBlade.webp", "level": "tartarus" },
-  { "itemId": "vase", "x": 30, "y": 18, "icon": "images/ItemVase.webp", "level": "surface" }
-]
